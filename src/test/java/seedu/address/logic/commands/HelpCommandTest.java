c80eb7bee78c868b70ef488dc1dd8f02cc4ce05e
daohang <wangdaohang123@gmail.com>
2025-10-09 21:25:28 +0800

    update test cases in help command

--- src/test/java/seedu/address/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
 
@@ -16,5 +17,11 @@ public class HelpCommandTest {
     public void execute_help_success() {
         CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE, false, false);
         assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void requiresWrite_returnsFalse() {
+        HelpCommand helpCommand = new HelpCommand();
+        assertFalse(helpCommand.requiresWrite());
     }
 }
