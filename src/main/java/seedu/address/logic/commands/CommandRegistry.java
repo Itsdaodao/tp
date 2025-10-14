93693870ff0f46f59adf14186e046a50c0f3214b
Harjun751 <Harjun751@gmail.com>
2025-10-14 19:12:48 +0800

    Create central registry for Commands
    There is no central way to access all commands in the application -
    parser uses a large switch case to handle command matching.
    
    Autocomplete requires a list of all command words. Creating and using
    a central repository of commands instead of manually listing them
    makes it easier to extend and manage the applications' commands.
    
    Let's:
    * Create a central command registry
    * Create a CommandFactory interface
    * Populate the registry with commands and factory methods
    * Modify ABParser to use the central repository instead of
    listing switch cases.

---
+++ src/main/java/seedu/address/logic/commands/CommandRegistry.java
@@ -0,0 +1,46 @@
+package seedu.address.logic.commands;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import seedu.address.logic.parser.AddCommandParser;
+import seedu.address.logic.parser.DeleteCommandParser;
+import seedu.address.logic.parser.EditCommandParser;
+import seedu.address.logic.parser.FindCommandParser;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Central Registry for all commands in the application.
+ */
+public class CommandRegistry {
+    private static final Map<String, CommandFactory> commands;
+
+    // Instantiate the command map
+    static {
+        commands = new HashMap<>();
+        commands.put(AddCommand.COMMAND_WORD, (args) -> new AddCommandParser().parse(args));
+        commands.put(EditCommand.COMMAND_WORD, (args) -> new EditCommandParser().parse(args));
+        commands.put(DeleteCommand.COMMAND_WORD, (args) -> new DeleteCommandParser().parse(args));
+        commands.put(ClearCommand.COMMAND_WORD, (args) -> new ClearCommand());
+        commands.put(FindCommand.COMMAND_WORD, (args) -> new FindCommandParser().parse(args));
+        commands.put(ListCommand.COMMAND_WORD, (args) -> new ListCommand());
+        commands.put(ExitCommand.COMMAND_WORD, (args) -> new ExitCommand());
+        commands.put(HelpCommand.COMMAND_WORD, (args) -> new HelpCommand());
+    }
+
+    /**
+     * Returns a set of all valid command words in the application.
+     */
+    public static Set<String> getCommandWords() {
+        return commands.keySet();
+    }
+
+    /**
+     * Returns the CommandFactory associated with the given command word.
+     * Returns null if the command word is not recognized.
+     */
+    public static CommandFactory getCommandFactory(String commandWord) {
+        return commands.get(commandWord);
+    }
+}
