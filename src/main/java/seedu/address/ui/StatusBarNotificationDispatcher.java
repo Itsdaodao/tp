118979773ad620e602e11d7deae22b7759c25ae1
Harjun751 <Harjun751@gmail.com>
2025-10-30 17:35:16 +0800

    Add StatusBarNotificationDispatcher
    No information is shown to the user if the AB data is corrupted.
    
    Create a NotificationDispatcher that can send messages to the
    bottom status bar. Add a section in MainApp initialization
    that sends a message if the addressbook is corrupted.

---
+++ src/main/java/seedu/address/ui/StatusBarNotificationDispatcher.java
@@ -0,0 +1,58 @@
+package seedu.address.ui;
+
+/**
+ * Handles dispatching information to the StatusBar in the application.
+ */
+public class StatusBarNotificationDispatcher {
+    private static StatusBarNotificationDispatcher instance;
+    private static String pendingMessages;
+    private static StatusBarFooter footer;
+
+    private StatusBarNotificationDispatcher() {
+
+    }
+
+    /**
+     * Returns the Singleton dispatcher instance
+     */
+    public static StatusBarNotificationDispatcher getInstance() {
+        if (instance == null) {
+            instance = new StatusBarNotificationDispatcher();
+        }
+        return instance;
+    }
+
+    /**
+     * Sets the StatusBarFooter to be used for dispatching messages.
+     * @param status The StatusBarFooter to be setting the messages on.
+     */
+    public void setStatusBar(StatusBarFooter status) {
+        footer = status;
+        if (pendingMessages != null) {
+            footer.setText(pendingMessages);
+            pendingMessages = null;
+        }
+    }
+
+    /**
+     * Dispatch a message to the StatusBar. If the StatusBar
+     * is not yet available, it enqueues the message to be shown later.
+     * @param text The text to be shown
+     */
+    public void notify(String text) {
+        if (footer == null) {
+            enqueueMessage(text);
+            return;
+        }
+        footer.setText(text);
+    }
+
+    private void enqueueMessage(String message) {
+        if (pendingMessages == null) {
+            pendingMessages = message;
+        } else {
+            pendingMessages = pendingMessages + "\n" + message;
+        }
+    }
+
+}
