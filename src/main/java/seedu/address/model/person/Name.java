4487804f8a5a0757fd6e2da430ac36a0cb2ea5ca
Wen-Cong <wencong.dev@gmail.com>
2025-11-03 15:17:29 +0800

    Update name validation to allow accent characters

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,13 +10,13 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters, slash and spaces, and it should not be blank";
+            "Name must start with a letter or number and contain only letters, numbers, spaces, or forward slashes.";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}/][\\p{Alnum}/ ]*";
+    public static final String VALIDATION_REGEX = "[\\p{L}\\p{N}][\\p{L}\\p{N}/ ]*";
 
     public final String fullName;
 
